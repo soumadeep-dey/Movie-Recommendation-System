58a4d1f142ff19537cd040112d76c67fec2692c8
soumadeep <contactsoumadeepdey@gmail.com>
2023-11-23 03:42:14 +0530

    set layout=wide, page_icon, page_title

--- app.py
+++ app.py
@@ -5,8 +5,8 @@ from dotenv import load_dotenv
 import os
 
 # Load Data
-movies_df = pickle.load(open('data/movies_df.pkl','rb'))
-similarity = pickle.load(open('data/similarity.pkl','rb'))
+movies_df = pickle.load(open('data/pkl_data/movies_df.pkl','rb'))
+similarity = pickle.load(open('data/pkl/similarity.pkl','rb'))
 
 # Function:
 def configure():
@@ -20,7 +20,7 @@ def fetch_poster(movie_id):
 def recommend(movie):
     movie_index = movies_df[movies_df['title'] == movie].index[0]
     distances = similarity[movie_index]
-    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:6]
+    movies_list = sorted(list(enumerate(distances)),reverse=True,key=lambda x:x[1])[1:11]
     
     recommended_movies = []
     recommended_movies_poster = []
@@ -31,37 +31,46 @@ def recommend(movie):
         recommended_movies_poster.append(fetch_poster(id))
     return recommended_movies,recommended_movies_poster
 
+
 # Web App
 def main():
     configure()
-    st.set_page_config(page_title="Movie Recommender", page_icon=":camera:",layout="wide")
-    
+    st.set_page_config(page_title="Movie Recommender", page_icon=":camera:", layout="wide")
     st.title('Movie Recommendation System')
-    selected_movie = st.selectbox("Select a movie",movies_df['title'])
+    selected_movie = st.selectbox(
+        label="Select a movie",
+        options=movies_df['title'],
+        placeholder = "Choose a movie",
+        index=None)
 
-    if st.button('Recommend'):
+
+    if st.button('Recommend'):            
         names,posters = recommend(selected_movie)
         col1, col2, col3, col4, col5 = st.columns(5)
         
         with col1:
             st.text(names[0])
-            st.image(posters[0])
+            st.image(posters[0],width=150)
+    
 
         with col2:
             st.text(names[1])
-            st.image(posters[1])
+            st.image(posters[1],width=150)
+
             
         with col3:
             st.text(names[2])
-            st.image(posters[2])
+            st.image(posters[2],width=150)
 
         with col4:
             st.text(names[3])
-            st.image(posters[3])
-        
+            st.image(posters[3],width=150)
+
         with col5:
             st.text(names[4])
-            st.image(posters[4])
+            st.image(posters[4],width=150)
 
+            
+            
 if __name__=="__main__": 
     main()
